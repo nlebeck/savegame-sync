c41a2d7ef0d110d560c4170270fe07c55dd45706
Niel Lebeck <niel.lebeck@gmail.com>
2016-11-23 17:39:42 -0800

    Create app, add OneDrive login, and start on UI design

---
+++ SavegameSync/SavegameSync/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.OneDrive.Sdk;
+using Microsoft.OneDrive.Sdk.Authentication;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SavegameSync
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private OneDriveClient client;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            loginToOneDrive();
+        }
+
+        private async void loginToOneDrive()
+        {
+            // I am not sure what best practices are regarding saving OAuth client IDs in code,
+            // so for now, I will leave the client ID out of the code that appears online
+            string clientId = "xxx";
+            string redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+            string[] scopes = { "onedrive.appfolder" };
+            var msaAuthenticationProvider = new MsaAuthenticationProvider(
+                clientId,
+                redirectUrl,
+                scopes,
+                /*CredentialCache*/ null,
+                new CredentialVault(clientId));
+            await msaAuthenticationProvider.RestoreMostRecentFromCacheOrAuthenticateUserAsync();
+            client = new OneDriveClient("https://api.onedrive.com/v2.0", msaAuthenticationProvider);
+            loginStatusTextBlock.Text = "Logged in";
+        }
+    }
+}
