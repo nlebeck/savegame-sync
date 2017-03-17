c1066b9b6c2f06f1524d92fbf6512c4ac6923472
Niel Lebeck <niel.lebeck@gmail.com>
2017-03-16 21:03:27 -0700

    Switch to Google Drive

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # savegame-sync
-A utility to synchronize savegame files for GOG games with OneDrive.
-
-## On hold
-
-This project is on hold until I find a solution for bugs associated with using Streams with the OneDrive SDK (described in this issue: https://github.com/OneDrive/onedrive-sdk-csharp/issues/204).
+A utility to synchronize savegame files for GOG games using Google Drive.
 
 ## Goal
 
-The goal of this project is to make a lightweight application that allows users to easily synchronize their savegame files for GOG games. Right now, GOG does not provide a cloud save utility like Steam Cloud, so users have to manually copy savegame files between computers. This application will maintain cloud saves in a user's OneDrive account and provide a simple interface for synchronizing those save files with local files.
+The goal of this project is to make a lightweight application that allows users
+to easily synchronize their savegame files for GOG games. Right now, GOG does
+not provide a cloud save utility like Steam Cloud, so users have to manually
+copy savegame files between computers. This application will maintain cloud
+saves in a user's Google Drive account and provide a simple interface for
+synchronizing those save files with local files.
 
 ## Plan
 
-1. First, I'll make a simple utility that allows the user to copy save files for a particular game (Medal of Honor: Allied Assault) to and from the user's OneDrive account.
-2. Then, I'll work on making the game-specific functionality extensible, so that the program can be modified to support different games with a minimum of additional code.
-3. Finally, I'll think about how to make the synchronization process automatic while still protecting users' data at all costs.
+1. First, I'll make a simple utility that allows the user to copy save files
+for a particular game (Medal of Honor: Allied Assault) to and from the user's
+Google Drive account.
+2. Then, I'll work on making the game-specific functionality extensible, so
+that the program can be modified to support different games with a minimum of
+additional code.
+3. Finally, I'll think about how to make the synchronization process automatic
+while still protecting users' data at all costs.
+
+## Repository organization
+SavegameSync is a Visual Studio solution directory. The subdirectory
+SavegameSyncWPF holds the version of the app that I'm currently working on,
+while the SavegameSync subdirectory holds the UWP version of the app that I
+started on but abandoned due to difficulties fixing dependency issues between
+UWP and the Google Drive NuGet package.
+
+## OneDrive bugs
+
+I had originally planned to use OneDrive to sync data, but I ran into some bugs
+trying to create files in OneDrive from either the UWP or WPF version of my
+app. This issue describes the problems I encountered:
+https://github.com/OneDrive/onedrive-sdk-csharp/issues/204.
--- SavegameSync/SavegameSyncWPF/MainWindow.xaml.cs
+++ SavegameSync/SavegameSyncWPF/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.OneDrive.Sdk;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.OneDrive.Sdk;
 using Microsoft.OneDrive.Sdk.Authentication;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,7 +28,9 @@ namespace SavegameSyncWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private OneDriveClient client;
+        private static string applicationName = "Savegame Sync";
+
+        private DriveService service;
 
         public MainWindow()
         {
@@ -32,22 +40,68 @@ namespace SavegameSyncWPF
         protected async override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
-            await LoginToOneDrive();
+            await LoginToGoogleDrive();
         }
 
-        private async Task LoginToOneDrive()
+        /*
+         * Based on the code in the .NET Quickstart in the Google Drive API documentation
+         * (https://developers.google.com/drive/v3/web/quickstart/dotnet).
+         */
+        private async Task LoginToGoogleDrive()
         {
-            string clientId = System.Configuration.ConfigurationManager.AppSettings["clientId"];
-            string redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
-            string[] scopes = { "onedrive.appfolder" };
-            var msaAuthenticationProvider = new MsaAuthenticationProvider(
-                clientId,
-                redirectUrl,
-                scopes,
-                new CredentialVault(clientId));
-            await msaAuthenticationProvider.RestoreMostRecentFromCacheOrAuthenticateUserAsync();
-            client = new OneDriveClient("https://api.onedrive.com/v1.0", msaAuthenticationProvider);
-            loginStatusTextBlock.Text = "Logged in";
+            string[] scopes = { DriveService.Scope.DriveAppdata };
+
+            UserCredential credential;
+            using (var stream = new FileStream("google-drive-client-secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                credPath = System.IO.Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName,
+            });
+
+            List<string> parents = new List<string>();
+            parents.Add("appDataFolder");
+            var file = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = "savegame-list.txt",
+                Parents = parents
+            };
+            FilesResource.CreateRequest createRequest = service.Files.Create(file);
+            createRequest.Fields = "id";
+            await createRequest.ExecuteAsync();
+
+
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.Spaces = "appDataFolder";
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            IList<Google.Apis.Drive.v3.Data.File> files = (await listRequest.ExecuteAsync()).Files;
+            Console.WriteLine("Files:");
+            if (files != null && files.Count > 0)
+            {
+                foreach (var fileToPrint in files)
+                {
+                    Console.WriteLine("{0} ({1})", fileToPrint.Name, fileToPrint.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+            }
         }
     }
 }
