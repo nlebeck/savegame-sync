801019e38e0a0a0cdc604edb0f011f5e0f8c1c0b
Niel Lebeck <niel.lebeck@gmail.com>
2018-06-23 15:12:20 -0700

    Add util methods and tests for them

---
+++ SavegameSync/SavegameSync/SavegameSyncUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavegameSync
+{
+    public static class SavegameSyncUtils
+    {
+        public static string SerializeDateTime(DateTime dateTime)
+        {
+            return dateTime.Ticks.ToString();
+        }
+
+        public static DateTime DeserializeDateTime(string str)
+        {
+            long ticks = long.Parse(str);
+            DateTime dateTime = new DateTime(ticks);
+            return dateTime;
+        }
+
+        public static string GetSavegameFileNameFromGuid(Guid guid)
+        {
+            return guid.ToString() + ".zip";
+        }
+    }
+}
---
+++ SavegameSync/SavegameSyncTest/SavegameSyncUtilsTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavegameSync;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavegameSyncTest
+{
+    [TestClass]
+    public class SavegameSyncUtilsTest
+    {
+        [TestMethod]
+        public void TestDateTimeSerialization()
+        {
+            DateTime dateTime1 = DateTime.UtcNow;
+            Assert.AreEqual(dateTime1, SavegameSyncUtils.DeserializeDateTime(SavegameSyncUtils.SerializeDateTime(dateTime1)));
+
+            DateTime dateTime2 = new DateTime(1, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            Assert.AreEqual(SavegameSyncUtils.SerializeDateTime(dateTime2), "0");
+            Assert.AreEqual(dateTime2, SavegameSyncUtils.DeserializeDateTime("0"));
+
+            DateTime dateTime3 = new DateTime(1533, 10, 27, 10, 42, 55, DateTimeKind.Utc);
+            Assert.AreEqual(dateTime3, SavegameSyncUtils.DeserializeDateTime(SavegameSyncUtils.SerializeDateTime(dateTime3)));
+        }
+    }
+}
