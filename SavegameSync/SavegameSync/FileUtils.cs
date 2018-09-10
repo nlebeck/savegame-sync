9b3c71d9679e655b5d44c8e039b6124781e4af1f
Niel Lebeck <niel.lebeck@gmail.com>
2018-09-09 21:08:45 -0700

    Remove unused file util methods

--- SavegameSync/SavegameSync/FileUtils.cs
+++ SavegameSync/SavegameSync/FileUtils.cs
@@ -74,57 +74,5 @@ namespace SavegameSync
                 Directory.Delete(path, true);
             }
         }
-
-        //TODO: Figure out if this method is unnecessary
-        public static string GetPathSuffix(string path, string pathPrefix)
-        {
-            if (path.StartsWith(pathPrefix))
-            {
-                return path.Substring(pathPrefix.Length);
-            }
-            return null;
-        }
-
-        //TODO: Figure out if this method is unnecessary
-        public static string GetFullPath(string pathPrefix, string pathSuffix)
-        {
-            string sep = "";
-            if (!pathPrefix.EndsWith("\\") && !pathSuffix.StartsWith("\\"))
-            {
-                sep = "\\";
-            }
-            return pathPrefix + sep + pathSuffix;
-        }
-
-        //TODO: Figure out if this method is unnecessary
-        /// <summary>
-        /// Create a new directory at the given path, creating any necessary parent directories.
-        /// </summary>
-        public static void CreateDirectories(string dirPath)
-        {
-            //TODO: implement this
-        }
-
-        //TODO: Figure out if this method is unnecessary
-        public static string GetParentDirectoryPath(string path)
-        {
-            string[] split = path.Split('\\');
-            int fileNamePosition = split.Length - 1;
-            string fileName = split[fileNamePosition];
-            while (fileName.Count() == 0)
-            {
-                fileNamePosition--;
-                fileName = split[fileNamePosition];
-            }
-
-            int fileNameIndex = path.LastIndexOf(fileName);
-            string untrimmedPath = path.Substring(0, fileNameIndex);
-            string trimmedPath = untrimmedPath;
-            while (trimmedPath.EndsWith("\\"))
-            {
-                trimmedPath = trimmedPath.Substring(0, trimmedPath.Length - 1);
-            }
-            return trimmedPath;
-        }
     }
 }
