493573b01bd88710c57ba71d2630354eca059d7d
Niel Lebeck <niel.lebeck@gmail.com>
2019-01-13 16:35:15 -0800

    Update metadata in AssemblyInfo.cs files

--- SavegameSync/SavegameSync/Properties/AssemblyInfo.cs
+++ SavegameSync/SavegameSync/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SavegameSyncWPF")]
+[assembly: AssemblyTitle("SavegameSync")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SavegameSyncWPF")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("SavegameSync")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- SavegameSync/SavegameSyncTest/Properties/AssemblyInfo.cs
+++ SavegameSync/SavegameSyncTest/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SavegameSyncTest")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
